b7cced9ded4af921a660f399a4c8554df35379a1
ariya <ariyamanouchehri079@gmail.com>
2024-01-14 12:18:21 +0330

    test Controller

---
+++ src/test/java/com/example/learnmocktest/LearnMockTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.learnmocktest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnMockTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ src/test/java/com/example/learnmocktest/UserControllerTest.java
@@ -0,0 +1,63 @@
+package com.example.learnmocktest;
+
+import com.example.learnmocktest.learn.Exception.UserException;
+import com.example.learnmocktest.learn.User;
+import com.example.learnmocktest.learn.UserController;
+import com.example.learnmocktest.learn.UserService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(UserController.class)
+public class UserControllerTest {
+
+    @MockBean
+    private UserService userService;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    private User input;
+
+    private User output;
+
+    @BeforeEach
+    void setUp() throws Exception {
+        input = new User(1L, "ariya", "manouchehri", 21);
+        output = new User(1L, "ariya", "manouchehri", 21);
+    }
+
+    @Test
+    void addUserResponse200() throws Exception {
+        Mockito.when(userService.addUser(input)).thenReturn(output);
+        mockMvc.perform(post("/test/addUser")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(input)))
+                .andExpect(status().is2xxSuccessful());
+    }
+
+    @Test
+    void addUserResponse500() throws Exception {
+        Mockito.when(userService.addUser(any())).thenThrow(UserException.class);
+        mockMvc.perform(post("/test/addUser")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(input)))
+                .andExpect(status().isNotFound());
+    }
+
+
+
+}
